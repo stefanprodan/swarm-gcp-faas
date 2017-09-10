2ca0a4ae5b01ea03d1e615a6227814bb35023c2b
Stefan Prodan <stefan.prodan@gmail.com>
2017-09-11 02:09:38 +0300

    output the manager's public ip

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "workspace" {
   value = "${terraform.workspace}"
 }
 
+output "swarm_manager_ip" {
+  value = "${google_compute_instance.manager.0.network_interface.0.access_config.0.assigned_nat_ip}"
+}
+
 output "swarm_managers" {
   value = "${concat(google_compute_instance.manager.*.name, google_compute_instance.manager.*.network_interface.0.access_config.0.assigned_nat_ip)}"
 }
