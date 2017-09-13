5d5f6a09d1f0d660a85592569c72f1ec743318be
Stefan Prodan <stefan.prodan@gmail.com>
2017-09-13 14:50:08 +0300

    weave cloud stack deploy

---
+++ monitoring/weave-cloud.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+#export DOCKER_HOST=$(terraform output swarm_manager_ip)
+
+if [ "$1" == "prune" ]; then
+    docker stack rm weave
+    sleep 5
+    docker volume prune -f
+else
+    network="monitoring"
+    if [ ! "$(docker network ls --filter name=$network -q)" ];then
+        docker network create --attachable -d overlay  $network
+    fi
+
+    TOKEN=rh1dj7xbehb6zu1gynm9fjd1t65fhnqp
+    ADMIN_USER=admin
+    ADMIN_PASSWORD=admin
+    docker stack deploy -c weave-cloud.yml weave
+fi
